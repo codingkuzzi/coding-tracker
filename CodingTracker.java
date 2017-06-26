2b36577de6a05a17eec0a2041c7e35d26058313d
Anna Kuznetsova and Yokke Hargono <anna@>
2017-06-26 10:57:14 -0700

    change methods

--- CodingTracker.java
+++ CodingTracker.java
@@ -3,15 +3,22 @@ import java.io.Console;
 public class CodingTracker {
   public static void main(String[] args) {
     Console myConsole = System.console();
+    Integer timeTotal = 0;
     //System.out.println("How much time did you spent coding yesterday ?");
-    askWhatTimeYouProgramming("yesterday");
+    Integer codingInteger = askWhatTimeYouProgramming();
+    Integer newCodingInteger = askWhatTimeYouProgramming();
+    Integer anotherCodingInteger = askWhatTimeYouProgramming();
+    timeTotal += codingInteger;
+    timeTotal += newCodingInteger;
+    timeTotal += anotherCodingInteger;
+    System.out.println(timeTotal);
     //String answer1 = myConsole.readLine();
     //System.out.println("How much time did you spent coding two days ago ?");
-    askWhatTimeYouProgramming("two days ago");
+    // askWhatTimeYouProgramming("two days ago");
 
     //String answer2 = myConsole.readLine();
     //System.out.println("How much time did you spent coding today ?");
-    askWhatTimeYouProgramming("today");
+    // askWhatTimeYouProgramming("today");
 
   //  System.out.println("Your total amount of coding hours is " + stringFinalTotal + ".");
     //String answer3 = myConsole.readLine();
@@ -29,19 +36,21 @@ public class CodingTracker {
     //askWhatTimeYouProgramming("today");
     //System.out.println("Your total amount of coding hours is " + stringTotal + ".");
   }
-  public static void askWhatTimeYouProgramming(String day) {
+  public static Integer askWhatTimeYouProgramming() {
     Console myConsole = System.console();
-    System.out.println("How much time did you spent coding " + day + "?");
+    System.out.println("How much time did you spent coding ?");
     String answer = myConsole.readLine();
     Integer intAnswer = Integer.parseInt(answer);
-    Integer total = 0;
-    Integer finalTotal = total + intAnswer;
-    String stringFinalTotal = Integer.toString(finalTotal);
+    System.out.println("You coded for " + answer + " hours.");
+    return intAnswer;
+    // Integer total = 0;
+    // Integer finalTotal = total + intAnswer;
+    // String stringFinalTotal = Integer.toString(finalTotal);
     //Integer yourAnswer = Integer.parseInt(myConsole.readLine());
     //Integer total = 0
     //Integer total += yourAnswer;
   //  String stringTotal = Integer.toString(total);
-  System.out.println("Your total amount of coding hours is " + stringFinalTotal + ".");
+  // System.out.println("Your total amount of coding hours is " + stringFinalTotal + ".");
 
   }
 
